47f8143f73efef85b50564bad2db619acc0cb797
Abrar Adnan <abrar.adnan01@gmail.com>
2023-03-16 15:11:48 +0600

    updated flask

--- render_deployment/app.py
+++ render_deployment/app.py
@@ -5,54 +5,41 @@ import requests
 app = Flask(__name__, static_url_path='/static')
 
 dummy = {
-  "gaze_percentage": 100,
-  "face_emotion": {
-    "angry": 1.5938487265966819,
-    "disgust": 0.01240325793125735,
-    "fear": 47.68757721435881,
-    "happy": 45.6566903932219,
-    "sad": 3.1918139858520127,
-    "surprise": 0.7046129829605734,
-    "neutral": 1.1530537686272717
-  },
-  "text_emotion": [
+    'data': 
+    [
     {
-      "label": "anger",
-      "score": 0.02366134524345398
-    },
+    'gaze_percentage': 100.0, 
+    'face_emotion': 
+    {'angry': 3.149491550588401, 
+     'disgust': 0.012228088369097853, 
+     'fear': 32.72938076707516, 
+     'happy': 45.65146129768167, 
+     'sad': 16.40989816598511, 
+     'surprise': 0.701252994671032, 
+     'neutral': 1.3462875321412522}, 
+     'text_emotion': 
+     [
     {
-      "label": "disgust",
-      "score": 0.006093125324696302
-    },
+    'label': 'anger', 
+     'score': 0.02366134524345398}, 
+     {
+    'label': 'disgust', 
+    'score': 0.006093125324696302}, 
+    {'label': 'fear', 'score': 0.004496702458709478}, 
+    {'label': 'joy', 'score': 0.5762037634849548}, 
+    {'label': 'neutral', 'score': 0.2536906599998474}, 
+    {'label': 'sadness', 'score': 0.007046668324619532}, 
+    {'label': 'surprise', 'score': 0.12880778312683105}],
+    'transcription': " Hey, just want to tell you you're doing good You're doing your absolute best you're trying your absolute hardest and that's what really matters Don't be so hard on yourself You're doing amazing and if nobody told you lately, I'm proud of you", 
+    'text_sentiment': 
+    [
     {
-      "label": "fear",
-      "score": 0.004496702458709478
-    },
-    {
-      "label": "joy",
-      "score": 0.5762037634849548
-    },
-    {
-      "label": "neutral",
-      "score": 0.2536906599998474
-    },
-    {
-      "label": "sadness",
-      "score": 0.007046668324619532
-    },
-    {
-      "label": "surprise",
-      "score": 0.12880778312683105
+    'label': 'POSITIVE', 
+    'score': 0.9998641014099121}]}], 
+    'is_generating': False, 
+    'duration': 13.432801008224487, 
+    'average_duration': 139.37824936029386
     }
-  ],
-  "transcription": " Hey, just want to tell you you're doing good You're doing your absolute best you're trying your absolute hardest and that's what really matters Don't be so hard on yourself You're doing amazing and if nobody told you lately, I'm proud of you",
-  "text_sentiment": [
-    {
-      "label": "POSITIVE",
-      "score": 0.9998641014099121
-    }
-  ]
-}
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -61,7 +48,9 @@ def index():
         video = request.files['video'].read()
         output = getFocus(video)
         print(output)
-        return render_template('index.html', result=output["data"], duration = output["duration"],  show=True)
+        # output = dummy
+        # print(output['duration'])
+        return render_template('index.html', result=output['data'], duration = output['duration'], show=True)
     else:
         return render_template('index.html', show=False)
 

5a75a556dfce441752535118b6bb8e20dadd4cec
Abrar Adnan <abrar.adnan01@gmail.com>
2023-03-17 12:25:57 +0600

    updated render app

--- render_deployment/app.py
+++ render_deployment/app.py
@@ -1,58 +1,53 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import base64
 import requests
 
 app = Flask(__name__, static_url_path='/static')
 
-dummy = {
-    'data': 
-    [
+dummy ={'data': 
+        [
     {
     'gaze_percentage': 100.0, 
-    'face_emotion': 
-    {'angry': 3.149491550588401, 
-     'disgust': 0.012228088369097853, 
-     'fear': 32.72938076707516, 
-     'happy': 45.65146129768167, 
-     'sad': 16.40989816598511, 
-     'surprise': 0.701252994671032, 
-     'neutral': 1.3462875321412522}, 
-     'text_emotion': 
-     [
-    {
-    'label': 'anger', 
-     'score': 0.02366134524345398}, 
-     {
-    'label': 'disgust', 
-    'score': 0.006093125324696302}, 
-    {'label': 'fear', 'score': 0.004496702458709478}, 
-    {'label': 'joy', 'score': 0.5762037634849548}, 
-    {'label': 'neutral', 'score': 0.2536906599998474}, 
-    {'label': 'sadness', 'score': 0.007046668324619532}, 
-    {'label': 'surprise', 'score': 0.12880778312683105}],
+    'face_emotion':  {'disgust': 0.01222809309886645, 'fear': 32.72938005048605, 'surprise': 0.7012529174430417, 'neutral': 1.346287779555521, 'anger': 3.149491521476035, 'joy': 45.65146084172068, 'sadness': 16.409897820592235}, 
+    'text_emotion': [{'label': 'anger', 'score': 0.023661362007260323}, {'label': 'disgust', 'score': 0.006093132775276899}, {'label': 'fear', 'score': 0.004496699199080467}, {'label': 'joy', 'score': 0.5762036442756653}, {'label': 'neutral', 'score': 0.2536909878253937}, {'label': 'sadness', 'score': 0.0070466771721839905}, {'label': 'surprise', 'score': 0.12880752980709076}], 
     'transcription': " Hey, just want to tell you you're doing good You're doing your absolute best you're trying your absolute hardest and that's what really matters Don't be so hard on yourself You're doing amazing and if nobody told you lately, I'm proud of you", 
-    'text_sentiment': 
-    [
-    {
-    'label': 'POSITIVE', 
-    'score': 0.9998641014099121}]}], 
-    'is_generating': False, 
-    'duration': 13.432801008224487, 
-    'average_duration': 139.37824936029386
-    }
+    'text_sentiment': [{'label': 'POSITIVE', 'score': 0.9998641014099121}]}], 'is_generating': False, 'duration': 7.471308708190918, 'average_duration': 9.438956022262573}
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         video = request.files['video'].read()
-        output = getFocus(video)
-        print(output)
-        # output = dummy
+        data = getFocus(video)
+        print(data)
+        data = process_output(data)
+        print("doing something with video")
         # print(output['duration'])
-        return render_template('index.html', result=output['data'], duration = output['duration'], show=True)
+        data = data['data'][0]
+        gaze_percentage = data['gaze_percentage']
+        face_emotion = data['face_emotion']
+        text_emotion = data['text_emotion']
+        transcription = data['transcription']
+        text_sentiment = data['text_sentiment']
+        duration = dummy['duration']
+        return render_template('index.html', gaze_percentage = gaze_percentage, face_emotion= face_emotion, text_emotion = text_emotion, 
+                               text_sentiment = text_sentiment, transcription = transcription, duration = duration, show=True)
     else:
         return render_template('index.html', show=False)
+    
+def process_output(data):
+    # Update the face_emotion data
+    for item in data['data']:
+        emotions = item['face_emotion']
+        top_emotions = dict(sorted(emotions.items(), key=lambda x: x[1], reverse=True)[:3])
+        item['face_emotion'] = top_emotions
+
+    # Update the text_emotion data
+    for item in data['data']:
+        emotions = item['text_emotion']
+        top_emotions = sorted(emotions, key=lambda x: x['score'], reverse=True)[:3]
+        item['text_emotion'] = top_emotions
+    return data
 
 def getFocus(video):
     print("Getting Results...")
@@ -63,7 +58,7 @@ def getFocus(video):
         "https://abrar-adnan-speech-analyzer.hf.space/run/predict", 
         json={
                 "data": [
-                    {"name": "video.mp4", "data": ""},
+                    {"name": "video.mp4", "data": file_data_base64},
                     file_data_base64
                     ]}
     ).json()
